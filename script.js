31c0feba476c9390231546dd66bf3d88cc9ef448
HappyCodingMama <HappyCodingMama@gmail.com>
2020-07-19 15:45:50 +0900

    complete it

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const word = document.getElementById('word');
 const text = document.getElementById('text');
 const scoreEl = document.getElementById('score');
 const timeEl = document.getElementById('time');
-const endgameEl = document.getElementById('end-game');
+const endgameEl = document.getElementById('end-game-container');
 const settingsBtn = document.getElementById('settings-btn');
 const settings = document.getElementById('settings');
 const settingsForm = document.getElementById('setting-form');
@@ -24,6 +24,21 @@ let score = 0;
 //Init time
 let time = 10;
 
+// Set difficulty to value in ls or medium
+let difficulty = localStorage.getItem('difficulty') !== null ?
+localStorage.getItem('difficulty') : 'medium';
+
+
+//Set difficulty select value
+difficultySelect.value = 
+localStorage.getItem('difficulty') !== null ?
+localStorage.getItem('difficulty') : 'medium';
+
+//Focus on text on start
+text.focus();
+
+//Start counting down
+const timeInterval = setInterval(updateTime, 1000);
 
 //Generate random word from array
 function getRandomWord() {
@@ -31,8 +46,78 @@ function getRandomWord() {
 }
 
 //Add word to DOM
-function addWrodToDOM() {
+function addWordToDOM() {
     randomWord = getRandomWord();
-    word.innerHTML
+    word.innerHTML = randomWord;
 }
+
+//update score
+function updateScore() {
+    score++;
+    scoreEl.innerHTML = score;
+}
+
+//update time
+function updateTime() {
+    time--;
+    timeEl.innerHTML = time + 's';
+
+    if(time === 0) {
+        clearInterval(timeInterval);
+        //end game
+        gameOver();
+    }
+}
+
+
+//Game over, show end screen
+function gameOver() {
+    endgameEl.innerHTML = `
+    <h1>Time ran out</h1>
+    <p>Your final score is ${score}</p>
+    <button onclick="location.reload()">Reload</button>`;
+
+    endgameEl.style.display = 'flex';
+}
+
+
+addWordToDOM();
+
+//Event Listeners
+
+
+//Typing
+text.addEventListener('input', e => {
+    const insertedText = e.target.value;
+    
+    if(insertedText === randomWord) {
+        addWordToDOM();
+        updateScore();
+
+        // Clear
+        e.target.value = '';
+
+        if (difficulty === 'hard') {
+            time += 2;
+        } else if (difficulty === 'medium') {
+            time += 3;
+        } else {
+            time += 5;
+        }
+
+        updateTime();
+    }
+});
+
+
+// Settings btn click
+settingsBtn.addEventListener('click', () => 
+settings.classList.toggle('hide'));
+
+// Settings Select
+settingsForm.addEventListener('change', e => {
+    difficulty = e.target.value;
+    localStorage.setItem('difficulty', difficulty);
+
+});
 
